d8eec9330709b45d97525fe15f080f979b11e76a
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-06-20 21:53:52 +0800

    Update mix.exs with description

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Breakout.MixProject do
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      licenses: ["MIT"]
+      licenses: ["MIT"],
+      description: "The Breakout game implemented in Elixir."
     ]
   end
 
